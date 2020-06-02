a84df4039d9d1191e7de3e058c06cd1a3fbb3b24
rorvts <rorvts@gmail.com>
2020-06-02 19:44:36 +0200

    Images route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path";
 
 const routes = express.Router();
 
 routes.get("/", (request, response) => {
   return response.json({});
 });
+
+routes.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 export default routes;
