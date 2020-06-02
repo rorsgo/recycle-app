a05dc6c5d5dbc3ad255b35adfeb26ad2d56dd023
rorvts <rorvts@gmail.com>
2020-06-02 23:11:08 +0200

    Installing cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 const application = express();
 
+application.use(cors());
 application.use(express.json());
 application.use(routes);
 application.listen(3333);
