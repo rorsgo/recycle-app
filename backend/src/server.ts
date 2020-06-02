cf6879b9f238d0dc827dba7e0463eabf03b93da4
rorvts <rorvts@gmail.com>
2020-06-02 15:13:24 +0200

    Sqlite connection and organizing dirs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,32 +1,8 @@
 import express from "express";
+import routes from "./routes";
 
 const application = express();
+
 application.use(express.json());
-const users = [
-  "Rodrigo",
-  "Mario",
-  "Jonathan"
-]
-
-application.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = users.filter(user => user.includes(search));
-
-  return response.json(filteredUsers);
-});
-
-application.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-
-  return response.json(user);
-});
-
-application.post("/users", (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-  return response.json(data);
-});
+application.use(routes);
 application.listen(3333);
