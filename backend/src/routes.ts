aaf1823aebb9cdb48d0f12f0dd430227e6f0e372
rorvts <rorvts@gmail.com>
2020-06-02 21:52:56 +0200

    Register points route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,11 +8,53 @@ routes.get("/items", async (request, response) => {
   const items = await knex("items").select("*");
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image_url}`
     }
   });
   return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  const {
+    // image,
+    name,
+    email,
+    country,
+    city,
+    state,
+    latitude,
+    longitude,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("points").insert({
+    image: "image",
+    name,
+    email,
+    country,
+    city,
+    state,
+    latitude,
+    longitude
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    }
+  });
+
+  await trx("point_items").insert(pointItems);
+  await trx.commit();
+
+  return response.json(pointItems);
 });
 
 routes.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
