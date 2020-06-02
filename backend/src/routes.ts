439f522a23a3ee6eff83510b9ec7b087bac649e9
rorvts <rorvts@gmail.com>
2020-06-02 20:03:43 +0200

    List items route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import path from "path";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({});
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image_url}`
+    }
+  });
+  return response.json(serializedItems);
 });
 
 routes.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
