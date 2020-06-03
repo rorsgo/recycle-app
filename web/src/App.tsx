5317b57be25f2195a0c2b9eaebf6f3f2f1473e44
rorvts <rorvts@gmail.com>
2020-06-03 13:51:35 +0200

    Exploring react components, properties and state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from "./Header";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
   return (
-        <h1>Hello World</h1>
+    <div>
+      <Header title={"Counter"} />
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Increase</button>
+    </div>
   );
 }
 
