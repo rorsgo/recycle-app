8a8f3d73d52008513459c8dde9f6d8e48b7ab5de
rorvts <rorvts@gmail.com>
2020-06-08 13:00:48 +0200

    Home page structure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./src/pages/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Basic structure</Text>
-    </View>
+    <Home/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
