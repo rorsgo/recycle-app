cbc493ddc466fabd6f8e9279d4ab9ec16c5232e0
rorvts <rorvts@gmail.com>
2020-06-18 10:55:19 -0300

    Removing react-native-picker-select style dependencies

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { Feather as Icon } from "@expo/vector-icons"
 import { StyleSheet, View, Image, Text, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
-import RNPickerSelect, { defaultStyles } from "react-native-picker-select";
+import RNPickerSelect from "react-native-picker-select";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 
