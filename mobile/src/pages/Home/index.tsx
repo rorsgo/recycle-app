3c77c8ca331e0f86eb0a12eb98f5a11e7ad55b6f
rorvts <rorvts@gmail.com>
2020-06-18 10:53:05 -0300

    Main page select buttons and IBGE API integration

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,16 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { Feather as Icon } from "@expo/vector-icons"
 import { StyleSheet, View, Image, Text, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+import RNPickerSelect, { defaultStyles } from "react-native-picker-select";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+
+interface IBGEStateResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
 
 const Home = () => {
-  const [state, setState] = useState('');
-  const [city, setCity] = useState('');
+  const [state, setState] = useState("0");
+  const [stateInitials, setStateInitials] = useState<string[]>([]);
+  const [city, setCity] = useState("0");
+  const [selectedCity, setSelectedCity] = useState<string[]>([]);
   const navigation = useNavigation();
 
+  useEffect(() => {
+    axios.get<IBGEStateResponse[]>("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+      .then(response => {
+        const stateInitials = response.data.map(state => state.sigla);
+        setStateInitials(stateInitials);
+      })
+  }, []);
+
+  useEffect(() => {
+    if (state === "0") {
+      return;
+    }
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`)
+      .then(response => {
+        const citiesName = response.data.map(city => city.nome);
+        setSelectedCity(citiesName);
+      })
+  }, [state]);
+
+  function handleSelectedState(event: ChangeEvent<HTMLSelectElement>) {
+    const stateInitial = event.target.value;
+    setState(stateInitial);
+  }
+
+  function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+    const cityName = event.target.value;
+    setCity(cityName);
+  }
+
   function navigationToPoints() {
-    navigation.navigate("Points");
+    navigation.navigate("Points", {
+      state,
+      city
+    });
   }
 
   return (
@@ -26,21 +70,67 @@ const Home = () => {
             <Text style={styles.description}>We help people find collection points efficiently.</Text>
           </View>
         </View>
-        <View style={styles.footer}>
-          <TextInput
-            style={styles.input}
-            placeholder="State"
+        <View>
+          <RNPickerSelect
+            useNativeAndroidPickerStyle={false}
+            style={pickerSelectStyles}
+            placeholder={{
+              label: "Select a state"
+            }}
             value={state}
-            onChangeText={setState}
-            maxLength={2}
-            autoCapitalize="characters"
-            autoCorrect={false} />
-          <TextInput
-            style={styles.input}
-            placeholder="City"
+            Icon={() => {
+              return (
+                <View
+                  style={{
+                    position: 'absolute',
+                    backgroundColor: 'transparent',
+                    borderTopWidth: 10,
+                    borderTopColor: 'gray',
+                    borderRightWidth: 10,
+                    borderRightColor: 'transparent',
+                    borderLeftWidth: 10,
+                    borderLeftColor: 'transparent',
+                    marginVertical: 30,
+                    marginHorizontal: -35
+                  }}
+                />
+              );
+            }}
+            onValueChange={state => setState(state)}
+            items={stateInitials?.map(state => ({
+              label: state,
+              value: state
+            }))} />
+          <RNPickerSelect
+            useNativeAndroidPickerStyle={false}
+            style={pickerSelectStyles}
+            placeholder={{
+              label: "Select a city"
+            }}
             value={city}
-            onChangeText={setCity}
-            autoCorrect={false} />
+            Icon={() => {
+              return (
+                <View
+                  style={{
+                    position: 'absolute',
+                    backgroundColor: 'transparent',
+                    borderTopWidth: 10,
+                    borderTopColor: 'gray',
+                    borderRightWidth: 10,
+                    borderRightColor: 'transparent',
+                    borderLeftWidth: 10,
+                    borderLeftColor: 'transparent',
+                    marginVertical: 30,
+                    marginHorizontal: -35
+                  }}
+                />
+              );
+            }}
+            onValueChange={city => setCity(city)}
+            items={selectedCity?.map(city => ({
+              label: city,
+              value: city
+            }))} />
           <RectButton style={styles.button} onPress={navigationToPoints}>
             <View style={styles.buttonIcon}>
               <Icon name="arrow-right" color="#FFFFFF" size={24} />
@@ -83,13 +173,8 @@ const styles = StyleSheet.create({
     lineHeight: 24,
   },
 
-  footer: {},
-
-  select: {},
-
   input: {
     height: 60,
-    backgroundColor: '#FFF',
     borderRadius: 10,
     marginBottom: 8,
     paddingHorizontal: 24,
@@ -122,6 +207,33 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto_500Medium',
     fontSize: 16,
   }
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: '#322153',
+    fontWeight: "bold",
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: '#999999',
+    borderRadius: 8,
+    color: '#322153',
+    fontWeight: "bold",
+    paddingRight: 30, // to ensure the text is never behind the icon
+    marginBottom: 10,
+    marginVertical: 10
+  },
 });
 
 export default Home;
